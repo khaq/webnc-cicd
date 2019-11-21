59a6abf78be6ecf105969f39979b412b1c89890b
Kha Do <kha.do@quoine.com>
2019-11-21 10:16:19 +0700

    Fix

--- fibonacci.js
+++ fibonacci.js
@@ -10,7 +10,7 @@ module.exports = exports = function fibonacci(n) {
   for (let i = 2; i <= n; i++) {
     result = n1 + n2;
     n1 = n2;
-    // n2 = result;
+    n2 = result;
   }
   return result;
 };
