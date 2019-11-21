059b3be2fbf60d874ed007054a8c34bc3abb68da
Kha Do <44114471+khaq@users.noreply.github.com>
2019-11-21 10:24:26 +0700

    Add eslint (#4)

--- fibonacci.js
+++ fibonacci.js
@@ -1,13 +1,14 @@
-module.exports = exports = function fibonacci(n) {
+module.exports = function fibonacci(n) {
   if (n === 0) {
     return 0;
   }
   if (n === 1) {
     return 1;
   }
-  let n1 = 0, n2 = 1;
+  let n1 = 0;
+  let n2 = 1;
   let result = 0;
-  for (let i = 2; i <= n; i++) {
+  for (let i = 2; i <= n; i += 1) {
     result = n1 + n2;
     n1 = n2;
     n2 = result;
