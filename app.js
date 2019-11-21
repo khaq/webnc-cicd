f07dec4dbd2a176b3a6eb3e1c085c9a4944c9e76
Kha Do <44114471+khaq@users.noreply.github.com>
2019-11-21 10:01:17 +0700

    Add fibonacci function and router (#1)

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const fibonacci = require('./fibonacci');
 
 const app = express();
 
 app.get('/', (_req, res) => {
   res.json({
     message: 'Hello Web NC',
+  });
+});
+
+app.get('/fibonacci/:n', (req, res) => {
+  const { n } = req.params;
+  const result = fibonacci(Number(n));
+  res.json({
+    n,
+    result,
   });
 });
 
